27797062fe735ab938e1639f3c2c38e5a252b537
Y.W KIM <kyw624@gmail.com>
2023-07-29 16:44:20 +0900

    Style: 사용안하는 코드 제거

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -18,56 +18,6 @@ const ExpensesFilter = (props) => {
           <option value="2019">2019</option>
         </select>
       </div>
-      <div className="expenses-filter__chart">
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Jan</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Feb</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Mar</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Apr</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>May</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Jun</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Jul</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Aug</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Sep</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Oct</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Nov</p>
-        </div>
-        <div className="chart__month-wrap">
-          <progress className="progress" max="100" value="70" />
-          <p>Dec</p>
-        </div>
-      </div>
     </div>
   );
 };
