bb6fa00c0c762287291f7dde75c3dba028c23f1f
Y.W KIM <kyw624@gmail.com>
2023-08-19 18:52:24 +0900

    Feat: 유효성 검사 실패한 Input에 Focus 기능 구현

--- 06-유효성검사예제/src/components/Login/Login.js
+++ 06-유효성검사예제/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from 'react';
 
 import Card from '../UI/Card/Card';
 import Button from '../UI/Button/Button';
@@ -51,6 +57,9 @@ const Login = (props) => {
 
   const authCtx = useContext(AuthContext);
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -90,13 +99,24 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      // 이메일 유효성 검사 실패 시 이메일 인풋 포커스
+      // input focus를 호출해 바인딩 된 activate 함수 실행
+      emailInputRef.current.focus(); // input focus
+    } else {
+      // 비밀번호 유효성 검사 실패 시 비밀번호 인풋 포커스
+      passwordInputRef.current.focus(); // input focus
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           type="email"
           label="E-mail"
@@ -106,6 +126,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           type="password"
           label="Password"
@@ -115,7 +136,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
