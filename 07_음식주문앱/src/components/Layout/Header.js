0c8ba11d5c720f8d5b7b306d792d58a57982b66f
Y.W KIM <kyw624@gmail.com>
2023-08-22 20:18:20 +0900

    Add: 로직을 제외한 화면 구성 완료

---
+++ 07_음식주문앱/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+        {/* <button>Cart</button> 장바구니 */}
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ 07_음식주문앱/src/components/UI/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const Button = (props) => {
+  return <button type={props.type || 'button'}>{props.children}</button>;
+};
+
+export default Button;
