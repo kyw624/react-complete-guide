96ada97da7b3e889e60162ef49ec6ba8242e00c4
Y.W KIM <kyw624@gmail.com>
2023-08-22 20:50:36 +0900

    Add: Cart 컴포넌트 구현 전 주석으로 요구사항 파악 및 정리

---
+++ 07_음식주문앱/src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+// Cart 컴포넌트
+//   - Modal + Backdrop + Card
+//   1. 장바구니 항목 렌더링
+//     - ul, li
+//     - itemlist state (prop chains 4 이상 => Context 사용?)
+//     - 이 state는 장바구니 버튼 뱃지에도 개수 표시
+//   2. 항목별 수량 표시
+//     - 개수 증감 버튼
+//   3. 장바구니 나가기, 주문하기 버튼
+
+// MealItemForm.js
+//   1. Add 버튼 클릭 => input value만큼 Cart에 아이템 추가
