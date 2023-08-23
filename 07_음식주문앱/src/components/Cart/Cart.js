5f88c9b6dfb74c69f007641fec7914a15d11e25f
Y.W KIM <kyw624@gmail.com>
2023-08-23 16:32:05 +0900

    Add: Cart 컴포넌트, Modal 컴포넌트 작성

--- 07_음식주문앱/src/components/Cart/Cart.js
+++ 07_음식주문앱/src/components/Cart/Cart.js
@@ -10,3 +10,34 @@
 
 // MealItemForm.js
 //   1. Add 버튼 클릭 => input value만큼 Cart에 아이템 추가
+
+import React from 'react';
+
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
