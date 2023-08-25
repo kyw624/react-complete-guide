771cde571e060610406e628dbba6df3265ed75bd
Y.W KIM <kyw624@gmail.com>
2023-08-25 19:05:08 +0900

    Add: Context를 활용한 상태 관리 기능 추가
    - 장바구니 목록
    - 장바구니 아이템 추가/제거

---
+++ 07_음식주문앱/src/store/CartProvider.js
@@ -0,0 +1,103 @@
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM': {
+      const updatedTotalAmount =
+        state.totalAmount + action.payload.price * action.payload.amount;
+
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      let updatedItems;
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.payload.amount,
+        };
+
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(action.payload);
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
+
+    case 'REMOVE_ITEM': {
+      // amount === 1: 항목 제거
+      // amount > 1: 1씩 감소
+
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+      const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+      let updatedItems;
+
+      if (existingCartItem.amount === 1) {
+        // remove
+        updatedItems = state.items.filter((item) => item.id !== action.payload);
+      } else {
+        // decrease
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount - 1,
+        };
+
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
+
+  const handleAddItem = (item) => {
+    dispatch({ type: 'ADD_ITEM', payload: item });
+  };
+
+  const handleRemoveItem = (id) => {
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: handleAddItem,
+    removeItem: handleRemoveItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
