bd528a07ef4e77ea563ad29d0512c65f181608d5
Y.W KIM <kyw624@gmail.com>
2023-08-29 00:34:19 +0900

    Feat: 모든 자식 컴포넌트 재평가로 인한 불필요한 렌더링 예제

--- 08_리액트작동방식예제/src/App.js
+++ 08_리액트작동방식예제/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
 
   const handleShowParagraph = () => {
     setShowParagraph((prevState) => !prevState);
@@ -13,7 +16,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={handleShowParagraph}>Toggle Paragraph</Button>
     </div>
   );
