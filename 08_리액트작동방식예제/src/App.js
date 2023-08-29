95565b72e4ae8216fa5793dd5cd1b91874a4bbdf
Y.W KIM <kyw624@gmail.com>
2023-08-29 15:26:52 +0900

    Feat: React.memo, useCallback 예제

--- 08_리액트작동방식예제/src/App.js
+++ 08_리액트작동방식예제/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,17 +6,25 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
-  const handleShowParagraph = () => {
-    setShowParagraph((prevState) => !prevState);
+  const handleShowParagraph = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, [allowToggle]);
+
+  const handleAllowToggle = () => {
+    setAllowToggle(true);
   };
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={handleAllowToggle}>Allow Toggling</Button>
       <Button onClick={handleShowParagraph}>Toggle Paragraph</Button>
     </div>
   );
