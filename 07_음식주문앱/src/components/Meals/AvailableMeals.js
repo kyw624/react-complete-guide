e7e984be0ec9894df8f68e019613d0f4497aa0ee
Y.W KIM <kyw624@gmail.com>
2023-09-05 19:45:41 +0900

    Refactor: Firebase 실시간 데이터베이스와 연결해 더미 데이터 대체

--- 07_음식주문앱/src/components/Meals/AvailableMeals.js
+++ 07_음식주문앱/src/components/Meals/AvailableMeals.js
@@ -1,46 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and gre en...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(
+          'https://react-food-order-app-4c35a-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+        );
+
+        if (!response.ok) {
+          throw new Error('Response error!');
+        }
+
+        const data = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  let mealsList = <p>Found no meals.</p>;
+
+  if (meals.length > 0) {
+    mealsList = meals.map((meal) => (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
+  }
+
+  if (error) {
+    console.log(error);
+    mealsList = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    mealsList = <p>Loading...</p>;
+  }
 
   return (
     <section className={classes.meals}>
