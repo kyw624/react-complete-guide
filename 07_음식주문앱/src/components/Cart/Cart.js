f733f9b716e9edc42f3f6d257ad98bec72b14b1c
Y.W KIM <kyw624@gmail.com>
2023-09-05 20:06:38 +0900

    Feat: Checkout 컴포넌트 적용
    - 결제 완료된 주문 장바구니에서 초기화
    - 결제 완료 시 장바구니 모달 닫기
    - 결제 정보들로 임시 영수증 console로 출력 (추후 모달로 구현 예정)

--- 07_음식주문앱/src/components/Cart/Cart.js
+++ 07_음식주문앱/src/components/Cart/Cart.js
@@ -11,15 +11,17 @@
 // MealItemForm.js
 //   1. Add 버튼 클릭 => input value만큼 Cart에 아이템 추가
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
 import classes from './Cart.module.css';
+import Checkout from '../Checkout/Checkout';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -47,6 +49,39 @@ const Cart = (props) => {
     </ul>
   );
 
+  const handleCheckout = () => {
+    console.log('checkout');
+    setIsCheckout(true);
+  };
+
+  // 제출 시 영수증 정보
+  const handleSubmitButtonClick = (checkoutInfo) => {
+    console.log(cartCtx.items);
+
+    const [name, phoneNumber] = checkoutInfo;
+
+    const receiptItems = cartCtx.items.map((item) => {
+      return `${item.name} | x${item.amount} | $${item.price * item.amount}`;
+    });
+
+    // 임시 영수증
+    // 요구사항 모두 만족 후 영수증 모달 구현 예정
+    console.log('------------------------');
+    console.log(receiptItems.join('\n'));
+    console.log('총액:', `$${cartCtx.totalAmount.toFixed(2)}`);
+    console.log('------------------------');
+    console.log('결제자:', name);
+    console.log('전화번호:', phoneNumber);
+
+    // 주문서 데이터베이스에 POST
+
+    // 완료된 주문 리셋
+    cartCtx.resetItem();
+
+    // 장바구니 모달 닫은 후 영수증 모달 띄우기
+    props.onClose();
+  };
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -58,8 +93,14 @@ const Cart = (props) => {
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
+        {hasItems && (
+          <button className={classes.button} onClick={handleCheckout}>
+            Order
+          </button>
+        )}
       </div>
+      {/* <Checkout onSubmit={handleSubmitButtonClick} /> */}
+      {isCheckout && <Checkout onSubmit={handleSubmitButtonClick} />}
     </Modal>
   );
 };
