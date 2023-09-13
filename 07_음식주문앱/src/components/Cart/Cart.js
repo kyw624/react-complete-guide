f57632afa852c115911279dd7f09e39e8e3b7296
Y.W KIM <kyw624@gmail.com>
2023-09-14 01:45:09 +0900

    Refactor: 강의를 보고 몇가지 로직 수정

--- 07_음식주문앱/src/components/Cart/Cart.js
+++ 07_음식주문앱/src/components/Cart/Cart.js
@@ -22,6 +22,8 @@ import Checkout from '../Checkout/Checkout';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false); // 제출중
+  const [didSubmit, setDidSubmit] = useState(false); // 제출 유무
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -38,6 +40,70 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const handleSubmitOrder = async (userData) => {
+    setIsSubmitting(true);
+
+    const now = new Date();
+    const hh = now.getHours();
+    const mm = now.getMinutes();
+    const ss = now.getSeconds();
+
+    const orderDate = now.toISOString().split('T')[0];
+    const orderTime = `${hh < 10 ? `0${hh}` : hh}:${mm < 10 ? `0${mm}` : mm}:${
+      ss < 10 ? `0${ss}` : ss
+    }`;
+
+    const newOrderInfo = {
+      ...userData,
+      orderTime,
+      orderDate,
+    };
+
+    const response = await fetch(
+      'https://react-food-order-app-4c35a-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: newOrderInfo,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+
+    setIsSubmitting(false);
+    setDidSubmit(true);
+
+    // 완료된 주문 리셋
+    cartCtx.resetItem();
+  };
+
+  // // 제출 시 영수증 정보
+  // const handleSubmitButtonClick = (checkoutInfo) => {
+  //   const [name, phoneNumber] = checkoutInfo;
+
+  //   const receiptItems = cartCtx.items.map((item) => {
+  //     return `${item.name} | x${item.amount} | $${item.price * item.amount}`;
+  //   });
+
+  //   // 임시 영수증
+  //   // 요구사항 모두 만족 후 영수증 모달 구현 예정
+  //   console.log('------------------------');
+  //   console.log(receiptItems.join('\n'));
+  //   console.log('총액:', `$${cartCtx.totalAmount.toFixed(2)}`);
+  //   console.log('------------------------');
+  //   console.log('결제자:', name);
+  //   console.log('전화번호:', phoneNumber);
+
+  //   // 완료된 주문 리셋
+  //   cartCtx.resetItem();
+
+  //   // 장바구니 모달 닫은 후 영수증 모달 띄우기
+  //   props.onClose();
+  // };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -53,30 +119,6 @@ const Cart = (props) => {
     </ul>
   );
 
-  // 제출 시 영수증 정보
-  const handleSubmitButtonClick = (checkoutInfo) => {
-    const [name, phoneNumber] = checkoutInfo;
-
-    const receiptItems = cartCtx.items.map((item) => {
-      return `${item.name} | x${item.amount} | $${item.price * item.amount}`;
-    });
-
-    // 임시 영수증
-    // 요구사항 모두 만족 후 영수증 모달 구현 예정
-    console.log('------------------------');
-    console.log(receiptItems.join('\n'));
-    console.log('총액:', `$${cartCtx.totalAmount.toFixed(2)}`);
-    console.log('------------------------');
-    console.log('결제자:', name);
-    console.log('전화번호:', phoneNumber);
-
-    // 완료된 주문 리셋
-    cartCtx.resetItem();
-
-    // 장바구니 모달 닫은 후 영수증 모달 띄우기
-    props.onClose();
-  };
-
   const modalActions = (
     <div className={classes.actions}>
       <button className={classes['button--alt']} onClick={props.onClose}>
@@ -90,17 +132,38 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
       {isCheckout && (
-        <Checkout onCancel={props.onClose} onSubmit={handleSubmitButtonClick} />
+        <Checkout onCancel={props.onClose} onConfirm={handleSubmitOrder} />
       )}
       {!isCheckout && modalActions}
+    </>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- 07_음식주문앱/src/components/Checkout/Checkout.js
+++ 07_음식주문앱/src/components/Checkout/Checkout.js
@@ -1,14 +1,67 @@
-import React, { useContext, useState } from 'react';
+import React, { /*useContext,*/ useRef, useState } from 'react';
 
 import useInput from '../hooks/useInput';
-import CartContext from '../../store/cart-context';
+// import CartContext from '../../store/cart-context';
 import classes from './Checkout.module.css';
 
-const isEmpty = (value) => value !== '';
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
 const regExp = new RegExp(/^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/);
 const isPhoneNumber = (value) => regExp.test(value);
 
 const Checkout = (props) => {
+  // 체크박스 핸들링
+  const [isSigned, setIsSigned] = useState(false);
+  const [formInputsValidity, setFormInputValidity] = useState({
+    name: true,
+    phoneNumber: true,
+    postalCode: true,
+    signature: true,
+  });
+
+  const nameInputRef = useRef();
+  const phoneNumberInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const signatureInputRef = useRef();
+
+  const handleConfirm = (e) => {
+    e.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredPhoneNumber = phoneNumberInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredPhoneNumberIsValid = isPhoneNumber(enteredPhoneNumber);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+    const enteredSignatureIsValid = isSigned;
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      phoneNumber: enteredPhoneNumberIsValid,
+      postalCode: enteredPostalCodeIsValid,
+      signature: enteredSignatureIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredPhoneNumberIsValid &&
+      enteredPostalCodeIsValid &&
+      enteredSignatureIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // Submit cart data
+    props.onConfirm({
+      name: enteredName,
+      phoneNumber: enteredPhoneNumber,
+      postalCode: enteredPostalCode,
+      signature: isSigned,
+    });
+  };
+
   const {
     value: nameValue,
     isValid: nameIsValid,
@@ -25,136 +78,152 @@ const Checkout = (props) => {
     handleInputBlur: phoneInputBlur,
   } = useInput(isPhoneNumber);
 
-  // 체크박스 핸들링
-  const [isSigned, setIsSigned] = useState(false);
-
-  const handleCheckInput = (e) => {
+  const handleCheckInput = () => {
     setIsSigned((prevState) => !prevState);
-    console.log(isSigned);
   };
 
-  let formIsValid = false;
+  // let formIsValid = false;
 
-  if (nameIsValid && phoneIsValid && isSigned) {
-    formIsValid = true;
-  } else {
-    formIsValid = false;
-  }
+  // if (nameIsValid && phoneIsValid && isSigned) {
+  //   formIsValid = true;
+  // } else {
+  //   formIsValid = false;
+  // }
 
-  const cartCtx = useContext(CartContext);
+  // const cartCtx = useContext(CartContext);
 
-  const saveOrder = async (orderInfo) => {
-    const now = new Date();
-    const hh = now.getHours();
-    const mm = now.getMinutes();
-    const ss = now.getSeconds();
+  // const saveOrder = async (orderInfo) => {
+  //   const now = new Date();
+  //   const hh = now.getHours();
+  //   const mm = now.getMinutes();
+  //   const ss = now.getSeconds();
 
-    const orderDate = now.toISOString().split('T')[0];
-    const orderTime = `${hh < 10 ? `0${hh}` : hh}:${mm < 10 ? `0${mm}` : mm}:${
-      ss < 10 ? `0${ss}` : ss
-    }`;
+  //   const orderDate = now.toISOString().split('T')[0];
+  //   const orderTime = `${hh < 10 ? `0${hh}` : hh}:${mm < 10 ? `0${mm}` : mm}:${
+  //     ss < 10 ? `0${ss}` : ss
+  //   }`;
 
-    const newOrderInfo = {
-      ...orderInfo,
-      orderTime,
-      orderDate,
-    };
+  //   const newOrderInfo = {
+  //     ...orderInfo,
+  //     orderTime,
+  //     orderDate,
+  //   };
 
-    try {
-      await fetch(
-        'https://react-food-order-app-4c35a-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newOrderInfo),
-        }
-      );
-    } catch (error) {
-      console.log('Save order Error!');
-    }
-    return;
-  };
+  //   try {
+  //     await fetch(
+  //       'https://react-food-order-app-4c35a-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
+  //       {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({
+  //           user: newOrderInfo,
+  //           orderedItems: cartCtx.itetms,
+  //         }),
+  //       }
+  //     );
+  //   } catch (error) {
+  //     console.log('Save order Error!');
+  //   }
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (!formIsValid) {
-      return;
-    }
+  //   if (!formIsValid) {
+  //     return;
+  //   }
 
-    props.onSubmit([nameValue, phoneValue]); // 영수증 작성을 위한 정보
+  //   props.onSubmit([nameValue, phoneValue]); // 영수증 작성을 위한 정보
 
-    // 주문서 데이터베이스에 POST
-    const orderInfo = {
-      items: cartCtx.items,
-      totalAmount: +cartCtx.totalAmount.toFixed(2),
-      name: nameValue,
-      phoneNumber: phoneValue,
-    };
+  //   // 주문서 데이터베이스에 POST
+  //   const orderInfo = {
+  //     items: cartCtx.items,
+  //     totalAmount: +cartCtx.totalAmount.toFixed(2),
+  //     name: nameValue,
+  //     phoneNumber: phoneValue,
+  //   };
 
-    saveOrder(orderInfo);
-  };
+  //   saveOrder(orderInfo);
+  // };
+
+  const nameInputClasses = `${classes.input} ${
+    formInputsValidity.name ? '' : classes.invalid
+  }`;
+
+  const phoneNumberInputClasses = `${classes.input} ${
+    formInputsValidity.phoneNumber ? '' : classes.invalid
+  }`;
+
+  const postalCodeInputClasses = `${classes.input} ${
+    formInputsValidity.postalCode ? '' : classes.invalid
+  }`;
+
+  const signatureInputClasses = `${classes.input} ${
+    formInputsValidity.signature ? '' : classes.invalid
+  }`;
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={classes.form} onSubmit={handleConfirm}>
       <div className={classes.order}>📄Order Sheet</div>
       {/* Name: string */}
-      <div className={classes.input}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Name</label>
         <input
-          className={nameHasError ? classes.invalid : ''}
           type='text'
           id='name'
           onChange={nameInputChange}
           onBlur={nameInputBlur}
+          ref={nameInputRef}
           value={nameValue}
         />
-        {nameHasError && <p>이름을 입력해주세요.</p>}
+        {!formInputsValidity.name && <p>이름을 입력해주세요.</p>}
       </div>
 
       {/* Phone number: number */}
-      <div className={classes.input}>
+      <div className={phoneNumberInputClasses}>
         <label htmlFor='phone'>Tel</label>
         <input
-          className={phoneHasError ? classes.invalid : ''}
           type='text'
           id='phone'
           onChange={phoneInputChange}
           onBlur={phoneInputBlur}
+          ref={phoneNumberInputRef}
           value={phoneValue}
         />
-        {phoneHasError && (
+        {!formInputsValidity.phoneNumber && (
           <p>전화번호 형식에 어긋납니다. ex) 12(3)-123(4)-1234</p>
         )}
       </div>
 
       {/* Postal Code: string */}
-      <div className={classes.input}>
+      <div className={postalCodeInputClasses}>
         <label htmlFor='postal'>Postal Code</label>
-        <input
-          className={nameHasError ? classes.invalid : ''}
-          type='text'
-          id='postal'
-          onChange={nameInputChange}
-          onBlur={nameInputBlur}
-          value={nameValue}
-        />
-        {nameHasError && <p>우편번호를 입력해주세요.</p>}
+        <input type='text' id='postal' ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>우편번호를 입력해주세요. (5자리)</p>
+        )}
       </div>
 
       {/* Signature: checkbox */}
-      <div className={classes.input}>
+      <div className={signatureInputClasses}>
         <label htmlFor='signature'>Signature</label>
-        <input type='checkbox' id='signature' onClick={handleCheckInput} />
-        {!isSigned && <p>서명이 필요합니다.</p>}
+        <input
+          type='checkbox'
+          id='signature'
+          onClick={handleCheckInput}
+          ref={signatureInputRef}
+        />
+        {!formInputsValidity.signature && <p>서명이 필요합니다.</p>}
       </div>
 
-      <button type='button' onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button disabled={!formIsValid}>Submit</button>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Submit</button>
+      </div>
     </form>
   );
 };
