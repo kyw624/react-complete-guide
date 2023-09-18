69aec6e1fa33021372ea4e6dfe8861f1e03f9504
Y.W KIM <kyw624@gmail.com>
2023-09-18 19:19:28 +0900

    Feat: 카운터 토글, 5씩 증가 기능 추가 및 action type 상수화

--- 15_리덕스예제/src/components/Counter.js
+++ 15_리덕스예제/src/components/Counter.js
@@ -1,84 +1,97 @@
-import { Component } from 'react';
-import { useSelector, useDispatch, connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
+import { INCREMENT, INCREASE, DECREMENT, TOGGLE } from '../store';
 
-// const Counter = () => {
-//   const dispatch = useDispatch();
-//   const counter = useSelector((state) => state.counter);
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-//   const handleIncrement = () => {
-//     dispatch({ type: 'INCREMENT' });
+  const handleIncrement = () => {
+    dispatch({ type: INCREMENT });
+  };
+
+  const handleIncrease = () => {
+    dispatch({ type: INCREASE, payload: 5 });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: DECREMENT });
+  };
+
+  const handleToggleCounter = () => {
+    dispatch({ type: TOGGLE });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}{' '}
+      <div>
+        <button type='button' onClick={handleIncrement}>
+          Increment
+        </button>
+        <button type='button' onClick={handleIncrease}>
+          Increse by 5
+        </button>
+        <button type='button' onClick={handleDecrement}>
+          Decrement
+        </button>
+      </div>
+      <button onClick={handleToggleCounter}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// import { Component } from 'react';
+// import {connect} from 'react-redux';
+
+// class Counter extends Component {
+//   handleIncrement() {
+//     this.props.increment();
+//   }
+
+//   handleDecrement() {
+//     this.props.decrement();
+//   }
+
+//   handleToggleCounter() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button type='button' onClick={this.handleIncrement.bind(this)}>
+//             Increment
+//           </button>
+//           <button type='button' onClick={this.handleDecrement.bind(this)}>
+//             Decrement
+//           </button>
+//         </div>
+//         <button onClick={this.handleToggleCounter}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// // 상태를 받는 함수
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter, // key가 prop name
 //   };
-
-//   const handleDecrement = () => {
-//     dispatch({ type: 'DECREMENT' });
-//   };
-
-//   const handleToggleCounter = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//         <button type='button' onClick={handleIncrement}>
-//           Increment
-//         </button>
-//         <button type='button' onClick={handleDecrement}>
-//           Decrement
-//         </button>
-//       </div>
-//       <button onClick={handleToggleCounter}>Toggle Counter</button>
-//     </main>
-//   );
 // };
 
-// export default Counter;
+// // 액션을 받는 함수
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: 'INCREMENT' }),
+//     decrement: () => dispatch({ type: 'DECREMENT' }),
+//   };
+// };
 
-class Counter extends Component {
-  handleIncrement() {
-    this.props.increment();
-  }
-
-  handleDecrement() {
-    this.props.decrement();
-  }
-
-  handleToggleCounter() {}
-
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button type='button' onClick={this.handleIncrement.bind(this)}>
-            Increment
-          </button>
-          <button type='button' onClick={this.handleDecrement.bind(this)}>
-            Decrement
-          </button>
-        </div>
-        <button onClick={this.handleToggleCounter}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-
-// 상태를 받는 함수
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter, // key가 prop name
-  };
-};
-
-// 액션을 받는 함수
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch({ type: 'INCREMENT' }),
-    decrement: () => dispatch({ type: 'DECREMENT' }),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- 15_리덕스예제/src/store/index.js
+++ 15_리덕스예제/src/store/index.js
@@ -1,16 +1,40 @@
 const redux = require('redux');
 
+export const INCREMENT = 'INCREMENT';
+export const INCREASE = 'INCREASE';
+export const DECREMENT = 'DECREMENT';
+export const TOGGLE = 'TOGGLE';
+
 const initailState = {
   counter: 0,
+  showCounter: true,
 };
 
 const counterReducer = (state = initailState, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return { counter: state.counter + 1 };
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
 
-    case 'DECREMENT':
-      return { counter: state.counter - 1 };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.payload,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    case TOGGLE:
+      return {
+        ...state,
+        showCounter: !state.showCounter,
+      };
 
     default:
       return state;
