56ab8b5a223df63cc4dd0d780cb6fdc389afc46e
Y.W KIM <kyw624@gmail.com>
2023-09-21 00:24:00 +0900

    Add: Redux toolkit을 활용한 상태 관리 적용

--- 16_리덕스연습/src/components/Shop/ProductItem.js
+++ 16_리덕스연습/src/components/Shop/ProductItem.js
@@ -1,8 +1,23 @@
+import { useDispatch } from 'react-redux';
+
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { cartActions } from '../../store/cart';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const { id, title, price, description } = props;
+
+  const handleAddToCart = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +28,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
       </Card>
     </li>
