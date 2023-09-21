9f4680ed5269d326e1b03d7cc5292ad2db2e5347
Y.W KIM <kyw624@gmail.com>
2023-09-21 21:02:30 +0900

    Refactor: 액션 생성 함수 내부에서 비동기 처리가 이루어지도록 변경

--- 16_리덕스연습/src/App.js
+++ 16_리덕스연습/src/App.js
@@ -4,10 +4,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart';
 
-const endPoint =
+export const endPoint =
   'https://react-redux-tutorial-cd2e6-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
 let isInitial = true;
@@ -19,47 +19,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-
-      const response = await fetch(`${endPoint}/cart.json`, {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Send cart data successfully!',
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    // 함수를 디스패치
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
