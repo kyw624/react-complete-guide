c7cd1aa380e927a584eb63767f37b3f08f574bc2
Y.W KIM <kyw624@gmail.com>
2023-09-26 16:26:55 +0900

    Feat: useNavigate를 통한 강제 페이지 이동 예제

--- 17_라우팅예제/src/pages/Home.js
+++ 17_라우팅예제/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function handleNavigate() {
+    navigate('/products');
+  }
+
   return (
     <>
       <div>My Home Page</div>
+      <p>
+        Go to <Link to='/products'>the list of products</Link>
+      </p>
+      <p>
+        <button onClick={handleNavigate}>Navigate</button>
+      </p>
     </>
   );
 }
