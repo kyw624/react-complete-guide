5fe41942d8039259bc9e8455de1dd2c449b2975f
Y.W KIM <kyw624@gmail.com>
2023-09-26 15:56:00 +0900

    Style: NavLink를 통한 요소 활성화 스타일 적용

--- 17_라우팅예제/src/components/MainNavigation.js
+++ 17_라우팅예제/src/components/MainNavigation.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
-function MainNavigation(props) {
+function MainNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to='/'>Home</Link>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end // '/'를 포함하는 모든 경로들에 대해 활성화되는 것을 방지
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to='/products'>Products</Link>
+            <NavLink
+              to='/products'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
