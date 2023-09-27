f840ebb5f2238d5232987c9e12e4e49d45952fa1
Y.W KIM <kyw624@gmail.com>
2023-09-27 19:22:35 +0900

    Feat: 이벤트 상세 페이지에 현재 이벤트 ID 출력

--- 18_라우팅연습/frontend/src/pages/EventDetailPage.js
+++ 18_라우팅연습/frontend/src/pages/EventDetailPage.js
@@ -1,7 +1,12 @@
+import { useParams } from 'react-router-dom';
+
 function EventDetailPage() {
+  const params = useParams();
+
   return (
     <p>
       <h1>Event detail</h1>
+      Current event: {params.someId}
     </p>
   );
 }
