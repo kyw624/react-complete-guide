da3c4b34557c7a72cf7c47b2af9f3e9ca30dd2f0
Y.W KIM <kyw624@gmail.com>
2023-09-27 19:10:59 +0900

    Feat: 레이아웃에 메인 네비게이션 추가

--- 18_라우팅연습/frontend/src/pages/RootLayout.js
+++ 18_라우팅연습/frontend/src/pages/RootLayout.js
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
+import MainNavigation from '../components/MainNavigation';
+
 function RootLayout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
