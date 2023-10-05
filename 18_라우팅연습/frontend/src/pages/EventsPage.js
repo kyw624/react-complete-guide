39bad634ccd355d5b86aa53c39343957dac8a8eb
Y.W KIM <kyw624@gmail.com>
2023-10-05 20:57:13 +0900

    Update: defer 함수를 활용해 데이터 가져오는동안 fallback 콘텐츠 제공

--- 18_라우팅연습/frontend/src/pages/EventsPage.js
+++ 18_라우팅연습/frontend/src/pages/EventsPage.js
@@ -1,17 +1,23 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { defer, json, useLoaderData, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData(); // defer가 반환하는 Promise
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
@@ -22,6 +28,14 @@ export async function loader() {
     // json 형식의 데이터를 포함하는 Response 객체를 생성하는 함수.
     return json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(), // Promise를 반환
+  });
 }
