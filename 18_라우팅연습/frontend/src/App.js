5e4fc13e4e0d159295377745b2dcbad6e2324ab6
Y.W KIM <kyw624@gmail.com>
2023-10-05 16:05:06 +0900

    Update: 여러 라우트에 동일한 loader 데이터를 사용하기위해 로직 변경

--- 18_라우팅연습/frontend/src/App.js
+++ 18_라우팅연습/frontend/src/App.js
@@ -51,11 +51,14 @@ const router = createBrowserRouter([
           },
           {
             path: ':someId',
-            element: <EventDetailPage />,
+            id: 'event-detail',
             loader: eventDetailLoader,
+            children: [
+              { index: true, element: <EventDetailPage /> },
+              { path: 'edit', element: <EditEventPage /> },
+            ],
           },
           { path: 'new', element: <NewEventPage /> },
-          { path: ':someId/edit', element: <EditEventPage /> },
         ],
       },
     ],
