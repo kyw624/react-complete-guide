55ae67cbbf5f83516b3dcfee72666fdf6a11ff8b
Y.W KIM <kyw624@gmail.com>
2023-10-05 21:14:08 +0900

    Update: 지연 시킬 데이터들의 제어 예제

--- 18_라우팅연습/frontend/src/pages/EventDetailPage.js
+++ 18_라우팅연습/frontend/src/pages/EventDetailPage.js
@@ -1,18 +1,37 @@
-import { useRouteLoaderData, json, redirect } from 'react-router-dom';
+import { Suspense } from 'react';
+import {
+  useRouteLoaderData,
+  json,
+  redirect,
+  defer,
+  Await,
+} from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p stlye={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p stlye={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.someId;
-
+async function loadEvent(id) {
   const response = await fetch(`http://localhost:8080/events/${id}`);
 
   if (!response.ok) {
@@ -21,8 +40,35 @@ export async function loader({ request, params }) {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // }); // Response 객체 생성
+
+    // json 형식의 데이터를 포함하는 Response 객체를 생성하는 함수.
+    return json({ message: 'Could not fetch events.' }, { status: 500 });
+  } else {
+    const resData = await response.json();
+
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.someId;
+
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ request, params }) {
