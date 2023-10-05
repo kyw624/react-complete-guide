ebf2399a93deab592b891cf9fba4c0e653ac620a
Y.W KIM <kyw624@gmail.com>
2023-10-05 16:39:34 +0900

    Feat: 새 이벤트 추가하는 로직 추가

--- 18_라우팅연습/frontend/src/App.js
+++ 18_라우팅연습/frontend/src/App.js
@@ -28,7 +28,7 @@ import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from './pages/EventDetailPage';
-import NewEventPage from './pages/NewEventPage';
+import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import EventsRootLayout from './pages/EventsRootLayout';
 import ErrorPage from './pages/ErrorPage';
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEventPage /> },
             ],
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
