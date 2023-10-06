955b2d7d918c0cd5948ab18f85cc5ee98afc336b
Y.W KIM <kyw624@gmail.com>
2023-10-06 18:36:49 +0900

    Add: 인증 action 추가 및 유효성 검증 오류 UI 업데이트

--- 19_인증데모/frontend/src/pages/Authentication.js
+++ 19_인증데모/frontend/src/pages/Authentication.js
@@ -1,3 +1,5 @@
+import { json, redirect } from 'react-router-dom';
+
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -6,4 +8,44 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-// /auth
+export async function action({ request }) {
+  // URL에서 쿼리 가져오기
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json(
+      {
+        message: 'Unsupported mode.',
+      },
+      { status: 422 }
+    );
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
+
+  // 추후에 토큰 관리 추가
+
+  // 로그인 성공시 홈으로 리다이렉션
+  return redirect('/');
+}
