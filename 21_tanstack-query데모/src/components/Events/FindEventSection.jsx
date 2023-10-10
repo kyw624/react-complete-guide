45bc79ac12a185c1dd0a5179f5617f1b972e39f7
Y.W KIM <kyw624@gmail.com>
2023-10-10 17:02:15 +0900

    Refactor: Tanstack 쿼리를 적용해 데이터 관련 로직 개선

--- 21_tanstack-query데모/src/components/Events/FindEventSection.jsx
+++ 21_tanstack-query데모/src/components/Events/FindEventSection.jsx
@@ -1,26 +1,67 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchEvents } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
+import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { search: searchTerm }], // 특정 검색 결과만을 가져오기 위해
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title='An error occurred'
+        message={error.info?.message || 'Failed to fetch events.'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className='events-list'>
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
-    <section className="content-section" id="all-events-section">
+    <section className='content-section' id='all-events-section'>
       <header>
         <h2>Find your next event!</h2>
-        <form onSubmit={handleSubmit} id="search-form">
+        <form onSubmit={handleSubmit} id='search-form'>
           <input
-            type="search"
-            placeholder="Search events"
+            type='search'
+            placeholder='Search events'
             ref={searchElement}
           />
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
