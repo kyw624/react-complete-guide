c72d35d995cee8533bfe6ec281429e140150782a
Y.W KIM <kyw624@gmail.com>
2023-10-10 18:02:00 +0900

    Feat: 이벤트 생성 Request 추가

--- 21_tanstack-query데모/src/util/http.js
+++ 21_tanstack-query데모/src/util/http.js
@@ -1,3 +1,4 @@
+// 이벤트 가져오기
 export async function fetchEvents({ signal, searchTerm }) {
   let url = 'http://localhost:3000/events';
 
@@ -17,4 +18,26 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+// 이벤트 생성
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while create the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
 }
