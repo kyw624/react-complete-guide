7af71ca0a98d11cd25fde70d01bbced44262088c
Y.W KIM <kyw624@gmail.com>
2023-10-15 19:18:52 +0900

    Update: max 프로퍼티를 활용해 가져오는 쿼리 데이터 값 제한

--- 21_tanstack-query데모/src/util/http.js
+++ 21_tanstack-query데모/src/util/http.js
@@ -4,11 +4,15 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 
 // 이벤트 가져오기
-export async function fetchEvents({ signal, searchTerm }) {
+export async function fetchEvents({ signal, searchTerm, max }) {
   let url = 'http://localhost:3000/events';
 
-  if (searchTerm) {
+  if (searchTerm && max) {
+    url += `?max=${max}&max=${max}`;
+  } else if (searchTerm) {
     url += `?search=${searchTerm}`;
+  } else if (max) {
+    url += `?max=${max}`;
   }
 
   const response = await fetch(url, { signal });
