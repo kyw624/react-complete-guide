4742a217acd8e0c475f44662caae98e001e615d6
Y.W KIM <kyw624@gmail.com>
2023-10-15 19:57:53 +0900

    Feat: useIsFetching 훅으로 데이터 로딩 진행바 표시

--- 21_tanstack-query데모/src/components/Header.jsx
+++ 21_tanstack-query데모/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { useIsFetching } from '@tanstack/react-query'; // 데이터 가져오는지 확인
+
 export default function Header({ children }) {
+  const fetching = useIsFetching(); // 데이터를 가져오면 0, 아니면 더 큰 수
+
   return (
     <>
-      <div id="main-header-loading"></div>
-      <header id="main-header">
-        <div id="header-title">
+      <div id='main-header-loading'>{fetching > 0 && <progress />}</div>
+      <header id='main-header'>
+        <div id='header-title'>
           <h1>React Events</h1>
         </div>
         <nav>{children}</nav>
