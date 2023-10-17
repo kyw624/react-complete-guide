4f97c9badf0045272d34a4e4374b46c349f2f85c
Y.W KIM <kyw624@gmail.com>
2023-10-17 15:14:06 +0900

    Refactor: 페이지 컴포넌트의 유지보수가 용이하도록 개선

---
+++ 23_next데모/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://content.skyscnr.com/m/1bf0d1856ed075b1/original/czech-republic-prague-gettyimages-503372055.jpg?resize=1800px:1800px&quality=100'
+      title='첫번째 모임'
+      address='서울'
+      description='첫번째 모임입니다.'
+    />
+  );
+}
+
+export default MeetupDetails;
