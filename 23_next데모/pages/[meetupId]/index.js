5aefdd030b958b57e68e9443de600b3ac0c835cd
Y.W KIM <kyw624@gmail.com>
2023-10-18 16:56:48 +0900

    Update: 상세 페이지 데이터 가져와 렌더링

--- 23_next데모/pages/[meetupId]/index.js
+++ 23_next데모/pages/[meetupId]/index.js
@@ -1,34 +1,68 @@
-import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { MongoClient, ObjectId } from 'mongodb';
 
-function MeetupDetails() {
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { user } from '../../db';
+
+const { username, password } = user;
+
+function MeetupDetails(props) {
+  console.log(props.meetupData);
   return (
     <MeetupDetail
-      image='https://content.skyscnr.com/m/1bf0d1856ed075b1/original/czech-republic-prague-gettyimages-503372055.jpg?resize=1800px:1800px&quality=100'
-      title='첫번째 모임'
-      address='서울'
-      description='첫번째 모임입니다.'
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${username}:${password}@cluster0.sjwr7sg.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  // 모든 문서에서 id 필드만 가져오기
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId; // context로 url에 접근
 
+  const client = await MongoClient.connect(
+    `mongodb+srv://${username}:${password}@cluster0.sjwr7sg.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  // 하나의 문서를 찾는 메서드
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId), // DB에 저장된 객체 ID 형식으로 변환
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        // id: meetupId,
-        // image:,
-        // title:,
-        // description:,
-        // addres:,
+        id: selectedMeetup._id.toString(),
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        title: selectedMeetup.title,
+        description: selectedMeetup.description,
       },
     },
   };
