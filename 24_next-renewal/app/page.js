eca50d128354b3b2662d2859fccfdebe2e08212a
Y.W KIM <kyw624@gmail.com>
2024-06-30 20:06:33 +0900

    Add: 라우팅 설정

--- 24_next-renewal/app/page.js
+++ 24_next-renewal/app/page.js
@@ -1,9 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <p>
+        <Link href='/meals'>Go to meals</Link>
+      </p>
+      <p>
+        <Link href='/community'>Go to community</Link>
+      </p>
     </main>
   );
 }
