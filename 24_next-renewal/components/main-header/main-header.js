1f87016d9255b649ee3734e518a3762ec8fd3626
Y.W KIM <kyw624@gmail.com>
2024-07-06 17:54:28 +0900

    Refactor: 코드 일부분에 클라이언트 훅 사용을 위한 컴포넌트 분리

--- 24_next-renewal/components/main-header/main-header.js
+++ 24_next-renewal/components/main-header/main-header.js
@@ -1,16 +1,12 @@
-'use client';
-
 import Link from 'next/link';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 
 export default function MainHeader() {
-  const path = usePathname();
-
   return (
     <>
       <MainHeaderBackground />
@@ -23,22 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link
-                href='/meals'
-                className={
-                  path.startsWith('/meals') ? classes.active : undefined
-                }
-              >
-                Browse Meals
-              </Link>
+              <NavLink href={'/meals'}>Browse Meals</NavLink>
             </li>
             <li>
-              <Link
-                href='/community'
-                className={path === '/community' ? classes.active : undefined}
-              >
-                Foodies Community
-              </Link>
+              <NavLink href={'/community'}>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
