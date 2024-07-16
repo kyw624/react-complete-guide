88e763a1ab1acbfee724174fcf9db1b48ffef3f5
Y.W KIM <kyw624@gmail.com>
2024-07-16 13:38:41 +0900

    Chore: 에러 핸들링 테스트 코드 주석 처리

--- 24_next-renewal/lib/meals.js
+++ 24_next-renewal/lib/meals.js
@@ -5,7 +5,7 @@ const db = sql('meals.db');
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000)); // 로딩 테스트를 위해 추가
 
-  throw new Error('Loading meals failed'); // 에러 핸들링 테스트
+  // throw new Error('Loading meals failed'); // 에러 핸들링 테스트
 
   return db.prepare('SELECT * FROM meals').all();
 }
