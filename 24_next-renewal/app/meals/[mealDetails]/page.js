8aa87cc3d70a4f3ea3c273e1fb1e1e3e1248dd96
Y.W KIM <kyw624@gmail.com>
2024-07-17 17:57:27 +0900

    Update: 상세 페이지 구현

--- 24_next-renewal/app/meals/[mealDetails]/page.js
+++ 24_next-renewal/app/meals/[mealDetails]/page.js
@@ -1,8 +1,34 @@
+import Image from 'next/image';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
 export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealDetails);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meal Details Page</h1>
-      <p>{params.mealDetails}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
