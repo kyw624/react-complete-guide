4836d17e6cf1f7e6ffba3095ccde1019adc1c281
Y.W KIM <kyw624@gmail.com>
2024-07-18 19:44:20 +0900

    Feat: 음식 공유 기능 추가

--- 24_next-renewal/app/meals/share/page.js
+++ 24_next-renewal/app/meals/share/page.js
@@ -1,4 +1,7 @@
+'use client';
+
 import ImagePicker from '@/components/meals/image-picker';
+import { shareMeal } from '@/lib/action';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -12,7 +15,7 @@ export default function ShareMealPage() {
       </header>
 
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor='name'>Your name</label>
@@ -40,7 +43,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label='Your image' name='image' />
           <p className={classes.actions}>
             <button type='submit'>Share Meal</button>
           </p>
