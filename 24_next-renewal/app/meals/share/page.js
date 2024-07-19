e8ed79e260b57889847590fb437ca474e01b3120
Y.W KIM <kyw624@gmail.com>
2024-07-19 18:42:12 +0900

    Refactor: 클라이언트 Hook 사용으로 버튼 컴포넌트화 및 UX 개선

--- 24_next-renewal/app/meals/share/page.js
+++ 24_next-renewal/app/meals/share/page.js
@@ -1,6 +1,5 @@
-'use client';
-
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/action';
 import classes from './page.module.css';
 
@@ -45,7 +44,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label='Your image' name='image' />
           <p className={classes.actions}>
-            <button type='submit'>Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
